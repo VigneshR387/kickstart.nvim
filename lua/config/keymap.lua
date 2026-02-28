3bc57a28e259121dd45c8798f95e586d264da036
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-02-28 17:34:37 +0530

    Add new keymaps for  buffer,quit,lua & Set Snacks.keymap

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,6 +1,6 @@
 -- [[ Basic Keymaps ]]
 --  See `:help map()`
-local map = vim.keymap.set
+local map = Snacks.keymap.set
 -- Clear highlights on search when pressing <Esc> in normal mode
 
 -- Set <space> as the leader key
@@ -43,3 +43,20 @@ map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 -- map("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- map("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- map("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+-- quit
+map('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Quit All' })
+
+-- buffers
+map('n', '<S-h>', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
+map('n', '<S-l>', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
+map('n', '[b', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
+map('n', ']b', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
+map('n', '<leader>bb', '<cmd>e #<cr>', { desc = 'Switch to Other Buffer' })
+map('n', '<leader>`', '<cmd>e #<cr>', { desc = 'Switch to Other Buffer' })
+map('n', '<leader>bd', function() Snacks.bufdelete() end, { desc = 'Delete Buffer' })
+map('n', '<leader>bo', function() Snacks.bufdelete.other() end, { desc = 'Delete Other Buffers' })
+map('n', '<leader>bD', '<cmd>:bd<cr>', { desc = 'Delete Buffer and Window' })
+
+-- lua
+map({ 'n', 'x' }, '<leader>r', function() Snacks.debug.run() end, { desc = 'Run Lua', ft = 'lua' })
