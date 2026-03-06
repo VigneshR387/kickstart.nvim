22bf1ee7e82d6e125482a394d20130b0bbaea325
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-06 17:59:52 +0530

    Added Profiler

--- init.lua
+++ init.lua
@@ -1,5 +1,20 @@
--- [[Load Modules]]
+-- Snacks.Profiler
+-- Use `PROF=1 nvim` to  profile neovim startup
+if vim.env.PROF then
+  -- example for lazy.nvim
+  -- change this to the correct path for your plugin manager
+  local snacks = vim.fn.stdpath 'data' .. '/lazy/snacks.nvim'
+  vim.opt.rtp:append(snacks)
+  require('snacks.profiler').startup {
+    startup = {
+      event = 'VimEnter', -- stop profiler on this event. Defaults to `VimEnter`
+      -- event = "UIEnter",
+      -- event = "VeryLazy",
+    },
+  }
+end
 
+-- [[Load Modules]]
 require 'config.globals'
 require 'config.options'
 require 'config.lazy'
