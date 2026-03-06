16fb003ce06a28aef492da30daf192bdf669bc4c
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-06 21:06:32 +0530

    fix:Support Condition based formatting for markdown

--- lua/plugins/formatting/conform.lua
+++ lua/plugins/formatting/conform.lua
@@ -1,5 +1,7 @@
 return { -- Autoformat
   'stevearc/conform.nvim',
+  enabled = true,
+  lazy = true,
   event = { 'BufWritePre' },
   cmd = { 'ConformInfo' },
   keys = {
@@ -26,8 +28,28 @@ return { -- Autoformat
         }
       end
     end,
+    formatters = {
+      ['markdown-toc'] = {
+        condition = function(_, ctx)
+          for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+            if line:find '<!%-%- toc %-%->' then return true end
+          end
+        end,
+      },
+
+      ['markdownlint-cli2'] = {
+        condition = function(_, ctx)
+          local diag = vim.tbl_filter(function(d) return d.source == 'markdownlint' end, vim.diagnostic.get(ctx.buf))
+
+          return #diag > 0
+        end,
+      },
+    },
     formatters_by_ft = {
       lua = { 'stylua' },
+      markdown = { 'prettierd', 'markdownlint-cli2', 'markdown-toc' },
+      ['markdown.mdx'] = { 'prettierd', 'markdownlint-cli2', 'markdown-toc' },
+
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
