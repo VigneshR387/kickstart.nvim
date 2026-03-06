94f6509d883ea7135e51a3965dc11f0c24b2070c
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-07 00:02:09 +0530

    fix: Decide not to use opts.server for defining server

--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -18,11 +18,64 @@ return {
     -- Allows extra capabilities provided by blink.cmp
     'saghen/blink.cmp',
   },
-  opts = {
+
+  config = function()
+    -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
+    -- and elegantly composed help section, `:help lsp-vs-treesitter`
+
+    local snacks_words_group = vim.api.nvim_create_augroup('snacks-words', { clear = true })
+    -- disable Snacks.words when lsp detachs
+
+    vim.api.nvim_create_autocmd('LspDetach', {
+      group = snacks_words_group,
+      callback = function(event) Snacks.words.disable(event.buf) end,
+    })
+
     -- Enable the following language servers
     --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
     --  See `:help lsp-config` for information about keys and how to configure
-    servers = {
+    ---@type table<string, vim.lsp.Config>
+    local servers = {
+      -- clangd = {},
+      -- gopls = {},
+      -- pyright = {},
+      -- rust_analyzer = {},
+      --
+      -- Some languages (like typescript) have entire language plugins that can be useful:
+      --    https://github.com/pmizio/typescript-tools.nvim
+      --
+      -- But for many setups, the LSP (`ts_ls`) will work just fine
+      -- ts_ls = {},
+      jsonls = {
+        -- lazy-load schemastore when needed
+        before_init = function(_, new_config)
+          new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+          vim.list_extend(new_config.settings.json.schemas, require('schemastore').json.schemas())
+        end,
+        settings = {
+          json = {
+            format = {
+              enable = true,
+            },
+            validate = { enable = true },
+          },
+        },
+      },
+      yamlls = {
+        settings = {
+          yaml = {
+            schemaStore = {
+              enable = false,
+              url = '',
+            },
+            schemas = require('schemastore').yaml.schemas(),
+          },
+        },
+      },
+
+      bashls = {},
+
+      stylua = {}, -- Used to format Lua code
 
       -- Special Lua Config, as recommended by neovim help docs
       lua_ls = {
@@ -52,62 +105,7 @@ return {
           Lua = {},
         },
       },
-      jsonls = {
-        -- lazy-load schemastore when needed
-        before_init = function(_, new_config)
-          new_config.settings.json.schemas = new_config.settings.json.schemas or {}
-          vim.list_extend(new_config.settings.json.schemas, require('schemastore').json.schemas())
-        end,
-        settings = {
-          json = {
-            format = {
-              enable = true,
-            },
-            validate = { enable = true },
-          },
-        },
-      },
-      yamlls = {
-        settings = {
-          yaml = {
-            schemaStore = {
-              enable = false,
-              url = '',
-            },
-            schemas = require('schemastore').yaml.schemas(),
-          },
-        },
-      },
-
-      bashls = {},
-      stylua = {},
-      -- clangd = {},
-      -- gopls = {},
-      -- pyright = {},
-      -- rust_analyzer = {},
-
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`ts_ls`) will work just fine
-      -- ts_ls = {},
-    },
-  },
-  config = function(_, opts)
-    -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-    -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
-    local snacks_words_group = vim.api.nvim_create_augroup('snacks-words', { clear = true })
-    -- disable Snacks.words when lsp detachs
-
-    vim.api.nvim_create_autocmd('LspDetach', {
-      group = snacks_words_group,
-      callback = function(event) Snacks.words.disable(event.buf) end,
-    })
-
-    local servers = opts.servers or {}
-
+    }
     for name, server in pairs(servers) do
       vim.lsp.config(name, server)
       vim.lsp.enable(name)
