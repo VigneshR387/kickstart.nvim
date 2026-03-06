c700cacc201225ffd21e14e7e248c8257a65e142
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-06 21:06:02 +0530

    plugin: yanky

---
+++ lua/plugins/editor/yanky.lua
@@ -0,0 +1,18 @@
+return {
+  'gbprod/yanky.nvim',
+  enabled = true,
+  dependencies = { 'folke/snacks.nvim' },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  keys = {
+    { 'p', '<Plug>(YankyPutAfter)', mode = { 'n', 'x' } },
+    { 'P', '<Plug>(YankyPutBefore)', mode = { 'n', 'x' } },
+    { 'gp', '<Plug>(YankyGPutAfter)', mode = { 'n', 'x' } },
+    { 'gP', '<Plug>(YankyGPutBefore)', mode = { 'n', 'x' } },
+    { '<c-p>', '<Plug>(YankyPreviousEntry)', mode = 'n' },
+    { '<c-n>', '<Plug>(YankyNextEntry)', mode = 'n' },
+  },
+}
