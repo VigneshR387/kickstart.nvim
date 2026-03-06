1764b2fa7bab74f098ae8e8f8e6497218336f1e9
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-06 17:57:46 +0530

    New keymaps

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,13 +1,12 @@
 -- [[ Basic Keymaps ]]
 --  See `:help map()`
+
+local root = require 'utils.root'
 local map = Snacks.keymap.set
 -- Clear highlights on search when pressing <Esc> in normal mode
 
 --  See `:help hlsearch`
 map('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
--- Diagonsitic keymap
-map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -37,6 +36,18 @@ map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 -- map("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- map("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- map("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+-- lazy
+map('n', '<leader>l', '<cmd>Lazy<cr>', { desc = 'Lazy' })
+
+-- new file
+map('n', '<leader>fn', '<cmd>enew<cr>', { desc = 'New File' })
+
+-- save file
+map({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save File' })
+
+-- better indenting
+map('x', '<', '<gv')
+map('x', '>', '>gv')
 
 -- quit
 map('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Quit All' })
@@ -51,6 +62,67 @@ map('n', '<leader>`', '<cmd>e #<cr>', { desc = 'Switch to Other Buffer' })
 map('n', '<leader>bd', function() Snacks.bufdelete() end, { desc = 'Delete Buffer' })
 map('n', '<leader>bo', function() Snacks.bufdelete.other() end, { desc = 'Delete Other Buffers' })
 map('n', '<leader>bD', '<cmd>:bd<cr>', { desc = 'Delete Buffer and Window' })
-
+-- highlights under cursor
+map('n', '<leader>ui', vim.show_pos, { desc = 'Inspect Pos' })
+map('n', '<leader>uI', function()
+  vim.treesitter.inspect_tree()
+  vim.api.nvim_input 'I'
+end, { desc = 'Inspect Tree' })
 -- lua
 map({ 'n', 'x' }, '<leader>r', function() Snacks.debug.run() end, { desc = 'Run Lua', ft = 'lua' })
+
+-- git
+if vim.fn.executable 'lazygit' == 1 then
+  map('n', '<leader>gg', function() Snacks.lazygit { cwd = root.git() } end, { desc = 'Lazygit (Root Dir)' })
+  map('n', '<leader>gG', function() Snacks.lazygit() end, { desc = 'Lazygit (cwd)' })
+end
+map({ 'n', 'x' }, '<leader>gB', function() Snacks.gitbrowse() end, { desc = 'Git Browse (open)' })
+map({ 'n', 'x' }, '<leader>gY', function()
+  Snacks.gitbrowse { open = function(url) vim.fn.setreg('+', url) end, notify = false }
+end, { desc = 'Git Browse (copy)' })
+
+-- toggle options
+
+Snacks.toggle.option('spell', { name = 'Spelling' }):map '<leader>us'
+Snacks.toggle.option('wrap', { name = 'Wrap' }):map '<leader>uw'
+Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map '<leader>uL'
+Snacks.toggle.diagnostics():map '<leader>ud'
+Snacks.toggle.line_number():map '<leader>ul'
+Snacks.toggle.option('conceallevel', { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2, name = 'Conceal Level' }):map '<leader>uc'
+Snacks.toggle.option('showtabline', { off = 0, on = vim.o.showtabline > 0 and vim.o.showtabline or 2, name = 'Tabline' }):map '<leader>uA'
+Snacks.toggle.treesitter():map '<leader>uT'
+Snacks.toggle.option('background', { off = 'light', on = 'dark', name = 'Dark Background' }):map '<leader>ub'
+Snacks.toggle.dim():map '<leader>uD'
+Snacks.toggle.animate():map '<leader>ua'
+Snacks.toggle.indent():map '<leader>ug'
+Snacks.toggle.scroll():map '<leader>uS'
+Snacks.toggle.zen():map '<leader>uz'
+Snacks.toggle.profiler():map '<leader>dpp'
+Snacks.toggle.profiler_highlights():map '<leader>dph'
+if vim.lsp.inlay_hint then Snacks.toggle.inlay_hints():map '<leader>uh' end
+
+-- windows
+map('n', '<leader>-', '<C-W>s', { desc = 'Split Window Below', remap = true })
+map('n', '<leader>|', '<C-W>v', { desc = 'Split Window Right', remap = true })
+map('n', '<leader>wd', '<C-W>c', { desc = 'Delete Window', remap = true })
+Snacks.toggle.zoom():map('<leader>wm'):map '<leader>uZ'
+-- tabs
+map('n', '<leader><tab>l', '<cmd>tablast<cr>', { desc = 'Last Tab' })
+map('n', '<leader><tab>o', '<cmd>tabonly<cr>', { desc = 'Close Other Tabs' })
+map('n', '<leader><tab>f', '<cmd>tabfirst<cr>', { desc = 'First Tab' })
+map('n', '<leader><tab><tab>', '<cmd>tabnew<cr>', { desc = 'New Tab' })
+map('n', '<leader><tab>]', '<cmd>tabnext<cr>', { desc = 'Next Tab' })
+map('n', '<leader><tab>d', '<cmd>tabclose<cr>', { desc = 'Close Tab' })
+map('n', '<leader><tab>[', '<cmd>tabprevious<cr>', { desc = 'Previous Tab' })
+
+-- floating terminal
+map('n', '<leader>fT', function() Snacks.terminal() end, { desc = 'Terminal (cwd)' })
+map('n', '<leader>ft', function() Snacks.terminal(nil, { cwd = root() }) end, { desc = 'Terminal (Root Dir)' })
+map({ 'n', 't' }, '<c-/>', function() Snacks.terminal(nil, { cwd = root() }) end, { desc = 'Terminal (Root Dir)' })
+map({ 'n', 't' }, '<c-_>', function() Snacks.terminal(nil, { cwd = root() }) end, { desc = 'which_key_ignore' })
+
+-- -- location list
+map('n', '<leader>xl', function()
+  local success, err = pcall(vim.fn.getloclist(0, { winid = 0 }).winid ~= 0 and vim.cmd.lclose or vim.cmd.lopen)
+  if not success and err then vim.notify(err, vim.log.levels.ERROR) end
+end, { desc = 'Location List' })
