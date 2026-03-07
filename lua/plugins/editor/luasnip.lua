14a9b575e0c755b55c23cac5815ab4a181d744c3
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-07 17:01:38 +0530

    plugin: Add LuaSnip

---
+++ lua/plugins/editor/luasnip.lua
@@ -0,0 +1,7 @@
+return {
+  'L3MON4D3/LuaSnip',
+  -- follow latest release.
+  dependencies = { 'rafamadriz/friendly-snippets' },
+  -- install jsregexp (optional!).
+  build = 'make install_jsregexp',
+}
