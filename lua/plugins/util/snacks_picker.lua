d7b4513be65b5b3939487d584d5be6e7fa6f86c8
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-08 00:32:07 +0530

    feat(picker): Add New keymaps

--- lua/plugins/util/snacks_picker.lua
+++ lua/plugins/util/snacks_picker.lua
@@ -4,6 +4,7 @@ return {
     picker = {},
     explorer = {},
   },
+
   keys = {
     -- Top Pickers & Explorer
     { '<leader><space>', function() Snacks.picker.smart() end, desc = 'Smart Find Files' },
@@ -18,12 +19,18 @@ return {
     { '<leader>fp', function() Snacks.picker.projects() end, desc = 'Projects' },
     { '<leader>fr', function() Snacks.picker.recent() end, desc = 'Recent' },
     -- git
-    { '<leader>gb', function() Snacks.picker.git_branches() end, desc = 'Git Branches' },
-    { '<leader>gl', function() Snacks.picker.git_log() end, desc = 'Git Log' },
-    { '<leader>gL', function() Snacks.picker.git_log_line() end, desc = 'Git Log Line' },
+    { '<leader>gb', function() Snacks.picker.git_log_line() end, desc = 'Git Blame Line' },
+    {
+      '<leader>gc',
+      function() Snacks.picker.git_branches() end,
+      desc = 'Git Branches',
+    },
+    { '<leader>gl', function() Snacks.picker.git_log { cwd = Util.root.git() } end, desc = 'Git Log' },
+    { '<leader>gL', function() Snacks.picker.git_log() end, desc = 'Git Log (cwd)' },
     { '<leader>gs', function() Snacks.picker.git_status() end, desc = 'Git Status' },
     { '<leader>gS', function() Snacks.picker.git_stash() end, desc = 'Git Stash' },
     { '<leader>gd', function() Snacks.picker.git_diff() end, desc = 'Git Diff (Hunks)' },
+    { '<leader>gD', function() Snacks.picker.git_diff { base = 'origin', group = true } end, desc = 'Git Diff (origin)' },
     { '<leader>gf', function() Snacks.picker.git_log_file() end, desc = 'Git Log File' },
     -- gh
     { '<leader>gi', function() Snacks.picker.gh_issue() end, desc = 'GitHub Issues (open)' },
@@ -33,7 +40,8 @@ return {
     -- Grep
     { '<leader>sb', function() Snacks.picker.lines() end, desc = 'Buffer Lines' },
     { '<leader>sB', function() Snacks.picker.grep_buffers() end, desc = 'Grep Open Buffers' },
-    { '<leader>sg', function() Snacks.picker.grep() end, desc = 'Grep' },
+    { '<leader>sg', function() Util.pick.open('live_grep', { title = 'Grep' }) end, desc = 'Grep (Root Dir)' },
+    { '<leader>sG', function() Util.pick.open('live_grep', { root = false, title = 'Grep (cwd)' }) end, desc = 'Grep (cwd)' },
     { '<leader>sw', function() Snacks.picker.grep_word() end, desc = 'Visual selection or word', mode = { 'n', 'x' } },
     -- search
     { '<leader>s"', function() Snacks.picker.registers() end, desc = 'Registers' },
@@ -68,4 +76,19 @@ return {
     { '<leader>ss', function() Snacks.picker.lsp_symbols() end, desc = 'LSP Symbols' },
     { '<leader>sS', function() Snacks.picker.lsp_workspace_symbols() end, desc = 'LSP Workspace Symbols' },
   },
+  init = function()
+    local Util = require 'util'
+
+    Util.pick.register {
+      name = 'snacks',
+      open = function(command, opts) Snacks.picker[command](opts) end,
+      commands = {
+        files = 'files',
+        live_grep = 'grep',
+        buffers = 'buffers',
+        git_files = 'git_files',
+        grep = 'grep',
+      },
+    }
+  end,
 }
