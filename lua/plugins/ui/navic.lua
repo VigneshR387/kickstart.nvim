380fbb80898679fcb479f3cd01a9663908db44bc
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-08 23:12:56 +0530

    feat: Add navic plugin

---
+++ lua/plugins/ui/navic.lua
@@ -0,0 +1,29 @@
+return {
+  -- lsp symbol navigation for lualine. This shows where
+  -- in the code structure you are - within functions, classes,
+  -- etc - in the statusline.
+  {
+    'SmiteshP/nvim-navic',
+    lazy = true,
+    init = function() vim.g.navic_silence = true end,
+    opts = function()
+      Snacks.util.lsp.on({ method = 'textDocument/documentSymbol' }, function(buffer, client) require('nvim-navic').attach(client, buffer) end)
+      return {
+        separator = ' ',
+        highlight = true,
+        depth_limit = 5,
+        icons = Util.icons.all_icons,
+        lazy_update_context = true,
+      }
+    end,
+  },
+
+  -- lualine integration
+  {
+    'nvim-lualine/lualine.nvim',
+    optional = true,
+    opts = function(_, opts)
+      if not vim.g.trouble_lualine then table.insert(opts.sections.lualine_c, { 'navic', color_correction = 'dynamic' }) end
+    end,
+  },
+}
