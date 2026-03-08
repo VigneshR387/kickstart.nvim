8be7e2334af93d366b2a72637c088dea31af9cf4
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-08 12:15:31 +0530

    feat(util): New submodule plugins
    Add LazyFile event

---
+++ lua/util/plugin.lua
@@ -0,0 +1,21 @@
+local Meta = require 'lazy.core.meta'
+local Plugin = require 'lazy.core.plugin'
+
+---@class lazyvim.util.plugin
+local M = {}
+
+---@type string[]
+M.core_imports = {}
+M.handle_defaults = true
+
+M.lazy_file_events = { 'BufReadPost', 'BufNewFile', 'BufWritePre' }
+
+function M.lazy_file()
+  -- Add support for the LazyFile event
+  local Event = require 'lazy.core.handler.event'
+
+  Event.mappings.LazyFile = { id = 'LazyFile', event = M.lazy_file_events }
+  Event.mappings['User LazyFile'] = Event.mappings.LazyFile
+end
+
+return M
