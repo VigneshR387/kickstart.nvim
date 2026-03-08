b8bc320031681a0e9f2f09d8fa1b00585d5e4cda
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-08 10:37:48 +0530

    feat(which-key): New keymaps

--- lua/plugins/ui/which-key.lua
+++ lua/plugins/ui/which-key.lua
@@ -42,4 +42,16 @@ return { -- Useful plugin to show you pending keybinds.
       },
     },
   },
+  keys = {
+    {
+      '<leader>?',
+      function() require('which-key').show { global = false } end,
+      desc = 'Buffer Keymaps (which-key)',
+    },
+    {
+      '<c-w><space>',
+      function() require('which-key').show { keys = '<c-w>', loop = true } end,
+      desc = 'Window Hydra Mode (which-key)',
+    },
+  },
 }
