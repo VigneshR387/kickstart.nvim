ef53609263ec62b068edf679cf7091d6b99ca17e
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-09 21:00:12 +0530

    fix: Disable Exit terminal mode keymap due to wierd  issues with Yazi.nvim

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -14,7 +14,9 @@ map('n', '<Esc>', '<cmd>nohlsearch<CR>')
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- WARN: CAUSING ISSUES WITH YAZI.NVIM
+
+-- map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
 -- map('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
