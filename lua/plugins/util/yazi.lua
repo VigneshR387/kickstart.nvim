9c522dc819f2948d479132d717a30002d0471f82
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-09 20:38:23 +0530

    feat: Add Yazi plugin

---
+++ lua/plugins/util/yazi.lua
@@ -0,0 +1,57 @@
+---@type LazySpec
+return {
+  'VigneshR387/yazi.nvim',
+  enabled = true,
+  version = '*', -- use the latest stable version
+  event = 'VeryLazy',
+  dependencies = {
+    { 'nvim-lua/plenary.nvim', lazy = true },
+  },
+  keys = {
+    -- 👇 in this section, choose your own keymappings!
+    {
+      '<leader>e',
+      mode = { 'n', 'v' },
+      '<cmd>Yazi<cr>',
+      desc = 'Explorer Yazi (cwd dir)',
+    },
+    {
+      '<leader>fe',
+      mode = { 'n', 'v' },
+      '<cmd>Yazi<cr>',
+      desc = 'Explorer Yazi (cwd dir)',
+    },
+    {
+      -- Open in the current working directory
+      '<leader>E',
+      '<cmd>Yazi cwd<cr>',
+      desc = 'Explorer Yazi (root)',
+    },
+    {
+      -- Open in the current working directory
+      '<leader>fE',
+      '<cmd>Yazi cwd<cr>',
+      desc = 'Explorer Yazi (root)',
+    },
+    {
+      '<c-up>',
+      '<cmd>Yazi toggle<cr>',
+      desc = 'Resume the last yazi session',
+    },
+  },
+  ---@type YaziConfig | {}
+  opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
+    open_for_directories = false,
+    keymaps = {
+      show_help = '<f2>',
+    },
+  },
+  -- 👇 if you use `open_for_directories=true`, this is recommended
+  init = function()
+    -- mark netrw as loaded so it's not loaded at all.
+    --
+    -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+    vim.g.loaded_netrwPlugin = 1
+  end,
+}
