48c069d8730c1d3e1fcc7cafba75391fbcc87c7a
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-14 16:09:55 +0530

    fix: changed keymaps for snacks explorer

--- lua/plugins/editor/snacks_explorer.lua
+++ lua/plugins/editor/snacks_explorer.lua
@@ -7,6 +7,44 @@ return {
       enabled = true,
       replace_netrw = true, -- Replace netrw with the snacks explorer
       trash = true, -- Use the system trash when deleting files
+      git_status = true,
+      diagnostics = true,
+      win = {
+
+        list = {
+
+          keys = {
+            ['h'] = 'explorer_up',
+            ['l'] = 'confirm',
+            ['c'] = 'explorer_close', -- close directory
+            ['a'] = 'explorer_add',
+            ['d'] = 'explorer_del',
+            ['r'] = 'explorer_rename',
+            ['Y'] = 'explorer_copy',
+            ['x'] = 'explorer_move',
+            ['o'] = 'explorer_open', -- open with system application
+            ['P'] = 'toggle_preview',
+            ['y'] = { 'explorer_yank', mode = { 'n', 'x' } },
+            ['p'] = 'explorer_paste',
+            ['u'] = 'explorer_update',
+            ['<c-c>'] = 'tcd',
+            ['<leader>/'] = 'picker_grep',
+            ['<c-t>'] = 'terminal',
+            ['.'] = 'explorer_focus',
+            ['I'] = 'toggle_ignored',
+            ['H'] = 'toggle_hidden',
+            ['Z'] = 'explorer_close_all',
+            [']g'] = 'explorer_git_next',
+            ['[g'] = 'explorer_git_prev',
+            [']d'] = 'explorer_diagnostic_next',
+            ['[d'] = 'explorer_diagnostic_prev',
+            [']w'] = 'explorer_warn_next',
+            ['[w'] = 'explorer_warn_prev',
+            [']e'] = 'explorer_error_next',
+            ['[e'] = 'explorer_error_prev',
+          },
+        },
+      },
     },
   },
   keys = {
