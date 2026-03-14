21adeb79110ade133cc1370a567d1e969e38a351
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-14 19:55:47 +0530

    chore: map localleader to '\\'

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -2,4 +2,4 @@
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader = '\\'
