6df14819062ef0eefc0b1d8deba33dca38775561
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-14 22:46:35 +0530

    feat: add markview plugin

---
+++ lua/plugins/editor/markview.lua
@@ -0,0 +1,9 @@
+return -- For `plugins.lua` users.
+{
+  'OXY2DEV/markview.nvim',
+  enabled = false,
+  lazy = false,
+
+  -- Completion for `blink.cmp`
+  -- dependencies = { "saghen/blink.cmp" },
+}
