35f276e427e7e864fe6ec36289a11c666cd49bf4
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-14 17:59:11 +0530

    feat: Smart image paste with filetype check

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -9,3 +9,18 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function() vim.hl.on_yank() end,
 })
+
+-- Smart paste for images with filetype check
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'markdown', 'org', 'tex', 'html', 'typst', 'rst', 'asciidoc', 'wiki' },
+  callback = function(ev)
+    local function paste()
+      local pasted = require('img-clip').paste_image()
+      if not pasted then vim.cmd(string.format('normal! "%s%dp', vim.v.register, vim.v.count1)) end
+    end
+
+    vim.keymap.set('n', 'p', paste, { buffer = ev.buf, noremap = true, silent = true })
+  end,
+})
+
