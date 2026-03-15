7dfdf1b705f19f72c9240c5d6455ce778a258ba9
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-15 20:39:36 +0530

    feat(yanky): add new keymaps for yanky

--- lua/plugins/editor/yanky.lua
+++ lua/plugins/editor/yanky.lua
@@ -1,18 +1,51 @@
+-- Filename: ~/.config/nvim/lua/plugins/editor/yanky.lua
+-- ~/.config/nvim/lua/plugins/editor/yanky.lua
+--
+-- Link to github repo:
+-- https://github.com/gbprod/yanky.nvim
+
+-- better yank/paste
 return {
   'gbprod/yanky.nvim',
-  enabled = true,
-  dependencies = { 'folke/snacks.nvim' },
+  event = 'LazyFile',
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    system_clipboard = {
+      sync_with_ring = not vim.env.SSH_CONNECTION,
+    },
+    highlight = { timer = 150 },
   },
   keys = {
-    { 'p', '<Plug>(YankyPutAfter)', mode = { 'n', 'x' } },
-    { 'P', '<Plug>(YankyPutBefore)', mode = { 'n', 'x' } },
-    { 'gp', '<Plug>(YankyGPutAfter)', mode = { 'n', 'x' } },
-    { 'gP', '<Plug>(YankyGPutBefore)', mode = { 'n', 'x' } },
-    { '<c-p>', '<Plug>(YankyPreviousEntry)', mode = 'n' },
-    { '<c-n>', '<Plug>(YankyNextEntry)', mode = 'n' },
+    {
+      '<leader>p',
+      function()
+        if Util.pick.picker.name == 'telescope' then
+          require('telescope').extensions.yank_history.yank_history {}
+        elseif Util.pick.picker.name == 'snacks' then
+          Snacks.picker.yanky()
+        else
+          vim.cmd [[YankyRingHistory]]
+        end
+      end,
+      mode = { 'n', 'x' },
+      desc = 'Open Yank History',
+    },
+        -- stylua: ignore
+    { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank Text" },
+    { 'p', '<Plug>(YankyPutAfter)', mode = { 'n', 'x' }, desc = 'Put Text After Cursor' },
+    { 'P', '<Plug>(YankyPutBefore)', mode = { 'n', 'x' }, desc = 'Put Text Before Cursor' },
+    { 'gp', '<Plug>(YankyGPutAfter)', mode = { 'n', 'x' }, desc = 'Put Text After Selection' },
+    { 'gP', '<Plug>(YankyGPutBefore)', mode = { 'n', 'x' }, desc = 'Put Text Before Selection' },
+    { '[y', '<Plug>(YankyCycleForward)', desc = 'Cycle Forward Through Yank History' },
+    { ']y', '<Plug>(YankyCycleBackward)', desc = 'Cycle Backward Through Yank History' },
+    { ']p', '<Plug>(YankyPutIndentAfterLinewise)', desc = 'Put Indented After Cursor (Linewise)' },
+    { '[p', '<Plug>(YankyPutIndentBeforeLinewise)', desc = 'Put Indented Before Cursor (Linewise)' },
+    { ']P', '<Plug>(YankyPutIndentAfterLinewise)', desc = 'Put Indented After Cursor (Linewise)' },
+    { '[P', '<Plug>(YankyPutIndentBeforeLinewise)', desc = 'Put Indented Before Cursor (Linewise)' },
+    { '>p', '<Plug>(YankyPutIndentAfterShiftRight)', desc = 'Put and Indent Right' },
+    { '<p', '<Plug>(YankyPutIndentAfterShiftLeft)', desc = 'Put and Indent Left' },
+    { '>P', '<Plug>(YankyPutIndentBeforeShiftRight)', desc = 'Put Before and Indent Right' },
+    { '<P', '<Plug>(YankyPutIndentBeforeShiftLeft)', desc = 'Put Before and Indent Left' },
+    { '=p', '<Plug>(YankyPutAfterFilter)', desc = 'Put After Applying a Filter' },
+    { '=P', '<Plug>(YankyPutBeforeFilter)', desc = 'Put Before Applying a Filter' },
   },
 }
