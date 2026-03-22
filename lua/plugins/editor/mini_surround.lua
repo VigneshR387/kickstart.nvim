fd16680f05ea02af95f179ea7c4a975805e7a08c
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-22 23:35:45 +0530

    fix(mini-surround): remap the to gs<action> to avoid conflict with flash.nvim

--- lua/plugins/editor/mini_surround.lua
+++ lua/plugins/editor/mini_surround.lua
@@ -1,18 +1,34 @@
+-- Fast and feature-rich surround actions. For text that includes
+-- surrounding characters like brackets or quotes, this allows you
+-- to select the text inside, change or modify the surrounding characters,
+-- and more.
 return {
   'nvim-mini/mini.surround',
   event = 'VeryLazy',
-  version = false,
+  keys = function(_, keys)
+    -- Populate the keys based on the user's options
+    local opts = Util.opts 'mini.surround'
+    local mappings = {
+      { opts.mappings.add, desc = 'Add Surrounding', mode = { 'n', 'x' } },
+      { opts.mappings.delete, desc = 'Delete Surrounding' },
+      { opts.mappings.find, desc = 'Find Right Surrounding' },
+      { opts.mappings.find_left, desc = 'Find Left Surrounding' },
+      { opts.mappings.highlight, desc = 'Highlight Surrounding' },
+      { opts.mappings.replace, desc = 'Replace Surrounding' },
+      { opts.mappings.update_n_lines, desc = 'Update `MiniSurround.config.n_lines`' },
+    }
+    mappings = vim.tbl_filter(function(m) return m[1] and #m[1] > 0 end, mappings)
+    return vim.list_extend(mappings, keys)
+  end,
   opts = {
     mappings = {
-      add = 'sa', -- Add surrounding in Normal and Visual modes
-      delete = 'sd', -- Delete surrounding
-      find = 'sf', -- Find surrounding (to the right)
-      find_left = 'sF', -- Find surrounding (to the left)
-      highlight = 'sh', -- Highlight surrounding
-      replace = 'sr', -- Replace surrounding
-
-      suffix_last = 'l', -- Suffix to search with "prev" method
-      suffix_next = 'n', -- Suffix to search with "next" method
+      add = 'gsa', -- Add surrounding in Normal and Visual modes
+      delete = 'gsd', -- Delete surrounding
+      find = 'gsf', -- Find surrounding (to the right)
+      find_left = 'gsF', -- Find surrounding (to the left)
+      highlight = 'gsh', -- Highlight surrounding
+      replace = 'gsr', -- Replace surrounding
+      update_n_lines = 'gsn', -- Update `n_lines`
     },
   },
 }
