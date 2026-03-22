d21eee28dab8f32c4fd1da28f540468d87b6839e
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-22 23:34:49 +0530

    feat(grug-far): add keymap for to snr in current file and snr in visual range

--- lua/plugins/editor/grug-far.lua
+++ lua/plugins/editor/grug-far.lua
@@ -20,5 +20,18 @@ return {
       mode = { 'n', 'x' },
       desc = 'Search and Replace',
     },
+    {
+      '<leader>s1',
+      '<cmd>lua require("grug-far").open({ prefills = { paths = vim.fn.expand("%") } })<cr>',
+      mode = { 'v', 'n' },
+      desc = 'Search and Replace for the current file',
+    },
+
+    {
+      '<leader>sv',
+      function() require('grug-far').open { visualSelectionUsage = 'operate-within-range' } end,
+      mode = { 'n', 'x' },
+      desc = 'grug-far: Search within range',
+    },
   },
 }
