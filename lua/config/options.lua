a017dc0b4796d6d290d4b3900c422e76f7cdb876
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-24 16:56:20 +0530

    feat: set textwidth to 80  in options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -93,6 +93,7 @@ opt.wildmode = 'longest:full,full' -- Command-line completion mode
 opt.winminwidth = 5 -- Minimum window width
 opt.wrap = false -- Disable line wrap
 opt.breakindent = true
+opt.textwidth = 80
 
 -- Fix markdown indentation settings
 vim.g.markdown_recommended_style = 0
