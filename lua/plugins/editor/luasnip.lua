ed99c131265106b45784310d26519fe3994b2229
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-25 14:01:32 +0530

    feat(LuaSnip): load vscode snippets from snippets folder in nvim root

--- lua/plugins/editor/luasnip.lua
+++ lua/plugins/editor/luasnip.lua
@@ -13,8 +13,14 @@ return {
     },
   },
 
-  config = function()
+  opts = function()
     require('luasnip.loaders.from_lua').lazy_load {
+      paths = {
+        '~/.config/nvim/snippets',
+      },
+    }
+
+    require('luasnip.loaders.from_vscode').lazy_load {
       paths = {
         '~/.config/nvim/snippets',
       },
