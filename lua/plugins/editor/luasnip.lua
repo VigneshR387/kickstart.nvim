ff78d0126dfadee6e014721bd8b889f9e0f2ddba
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-29 20:34:10 +0530

    feat(Luasnip):Add keymap to jump between placeholders

--- lua/plugins/editor/luasnip.lua
+++ lua/plugins/editor/luasnip.lua
@@ -26,4 +26,33 @@ return {
       },
     }
   end,
+  keys = {
+    {
+      '<C-K>',
+      function() require('luasnip').expand() end,
+      mode = 'i',
+      silent = true,
+    },
+    {
+      '<C-L>',
+      function() require('luasnip').jump(1) end,
+      mode = { 'i', 's' },
+      silent = true,
+    },
+    {
+      '<C-J>',
+      function() require('luasnip').jump(-1) end,
+      mode = { 'i', 's' },
+      silent = true,
+    },
+    {
+      '<C-E>',
+      function()
+        local ls = require 'luasnip'
+        if ls.choice_active() then ls.change_choice(1) end
+      end,
+      mode = { 'i', 's' },
+      silent = true,
+    },
+  },
 }
