e0260b8a5c52f2a0cf84ba350e7e55df775cbd8b
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-29 23:23:14 +0530

    feat: add keymap to jump between markdown headings

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -622,6 +622,57 @@ map('n', '<leader>mm', function() Util.markdown.jump_to_toc() end, { desc = '[P]
 -- Mapping to return to the previously saved cursor position
 map('n', '<leader>mn', function() Util.markdown.jump_to_prev_cursor() end, { desc = '[P]Return to position before jumping' })
 
+-- Search UP for a markdown header
+-- Make sure to follow proper markdown convention, and you have a single H1
+-- heading at the very top of the file
+-- This will only search for H2 headings and above
+-- hardtime.nvim causes issues with this key, you have to unrestrict it in the
+-- plugin config
+vim.keymap.set({ 'n', 'v' }, 'gk', function()
+  -- `?` - Start a search backwards from the current cursor position.
+  -- `^` - Match the beginning of a line.
+  -- `##` - Match 2 ## symbols
+  -- `\\+` - Match one or more occurrences of prev element (#)
+  -- `\\s` - Match exactly one whitespace character following the hashes
+  -- `.*` - Match any characters (except newline) following the space
+  -- vim.cmd("silent! ?^##\\+\\s.*$")
+  local ft = vim.bo.filetype
+  if ft == 'typst' then
+    vim.cmd 'silent! ?^==\\+\\s.*$'
+    -- Clear the search highlight
+    vim.cmd 'nohlsearch'
+    return
+  end -- `$` - Match extends to end of line
+  vim.cmd 'silent! ?^##\\+\\s.*$'
+  -- Clear the search highlight
+  vim.cmd 'nohlsearch'
+end, { desc = '[P]Go to previous markdown header' })
+
+-- Search DOWN for a markdown header
+-- Make sure to follow proper markdown convention, and you have a single H1
+-- heading at the very top of the file
+-- This will only search for H2 headings and above
+-- hardtime.nvim causes issues with this key, you have to unrestrict it in the
+-- plugin config
+vim.keymap.set({ 'n', 'v' }, 'gj', function()
+  -- `/` - Start a search forwards from the current cursor position.
+  -- `^` - Match the beginning of a line.
+  -- `##` - Match 2 ## symbols
+  -- `\\+` - Match one or more occurrences of prev element (#)
+  -- `\\s` - Match exactly one whitespace character following the hashes
+  -- `.*` - Match any characters (except newline) following the space
+  -- `$` - Match extends to end of line
+  local ft = vim.bo.filetype
+  if ft == 'typst' then
+    vim.cmd 'silent! /^==\\+\\s.*$'
+    -- Clear the search highlight
+    vim.cmd 'nohlsearch'
+    return
+  end
+  vim.cmd 'silent! /^##\\+\\s.*$'
+  -- Clear the search highlight
+  vim.cmd 'nohlsearch'
+end, { desc = '[P]Go to next markdown header' })
 -- ###########################################################################################
 -- #                                    Markdown (END)
 -- ###########################################################################################
