5c23f9b779146e7b91aaed3995102ac16ee25ea0
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-03-29 15:38:09 +0530

    feat(markview): set code block label to left side

--- lua/plugins/editor/markview.lua
+++ lua/plugins/editor/markview.lua
@@ -17,6 +17,41 @@ return {
         -- headings = presets.headings.glow_center,
 
         tables = presets.tables.rounded,
+
+        code_blocks = {
+          enable = true,
+
+          border_hl = 'MarkviewCode',
+          info_hl = 'MarkviewCodeInfo',
+
+          label_direction = 'left',
+          label_hl = nil,
+
+          min_width = 60,
+          pad_amount = 2,
+          pad_char = ' ',
+
+          default = {
+            block_hl = 'MarkviewCode',
+            pad_hl = 'MarkviewCode',
+          },
+
+          ['diff'] = {
+            block_hl = function(_, line)
+              if line:match '^%+' then
+                return 'MarkviewPalette4'
+              elseif line:match '^%-' then
+                return 'MarkviewPalette1'
+              else
+                return 'MarkviewCode'
+              end
+            end,
+            pad_hl = 'MarkviewCode',
+          },
+
+          style = 'block',
+          sign = true,
+        },
       },
     }
   end,
