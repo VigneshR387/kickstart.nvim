6a734a1d61629730c4e576596af02d56b0b6cb40
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-04-01 15:55:32 +0530

    fix(nvim-lspconfig): set correct directory path for loading dictionary in harper ls

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -98,7 +98,7 @@ return {
         filetypes = { 'markdown', 'typst' },
         settings = {
           ['harper-ls'] = {
-            userDictPath = '~/github/dotfiles-latest/neovim/neobean/spell/en.utf-8.add',
+            userDictPath = '~/.config/nvim/spell/en.utf-8.add',
             -- linters = {
             --   -- Disabling ToDoHyphen because of
             --   -- https://github.com/Automattic/harper/issues/1573#issuecomment-3777776431
@@ -116,6 +116,46 @@ return {
         },
       },
       vtsls = {},
+      mpls = {
+        cmd = {
+          'mpls',
+          '--no-auto',
+          '--theme',
+          'dark',
+          '--enable-emoji',
+          '--enable-footnotes',
+          -- "--enable-wikilinks",
+        },
+        root_markers = { '.marksman.toml', '.git' },
+        filetypes = { 'markdown' },
+        on_attach = function(client, bufnr)
+          vim.api.nvim_create_autocmd('BufEnter', {
+            pattern = { '*.md' },
+            group = vim.api.nvim_create_augroup('lspconfig.mpls.focus', { clear = true }),
+            callback = function(ctx)
+              ---@diagnostic disable-next-line:param-type-mismatch
+              client:notify('mpls/editorDidChangeFocus', { uri = ctx.match })
+            end,
+            desc = 'mpls: notify buffer focus changed',
+          })
+          vim.api.nvim_buf_create_user_command(
+            bufnr,
+            'LspMplsOpenPreview',
+            function()
+              client:exec_cmd {
+                title = 'Preview markdown with mpls',
+                command = 'open-preview',
+              }
+            end,
+            { desc = 'Preview markdown with mpls' }
+          )
+          -- Optional keybinding
+          vim.keymap.set('n', '<leader>mp', '<cmd>LspMplsOpenPreview<cr>', {
+            buffer = bufnr,
+            desc = 'Markdown Preview',
+          })
+        end,
+      },
     }
     for name, server in pairs(servers) do
       vim.lsp.config(name, server)
