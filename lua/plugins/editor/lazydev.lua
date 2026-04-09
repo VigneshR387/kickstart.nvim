d938993b51101cf1e16aa7b557b3014b1dcb9d8c
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-04-09 22:09:30 +0530

    feat(lazydev): add new libraries

--- lua/plugins/editor/lazydev.lua
+++ lua/plugins/editor/lazydev.lua
@@ -7,6 +7,8 @@ return {
         -- See the configuration section for more details
         -- Load luvit types when the `vim.uv` word is found
         { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+        { path = 'nvim-lspconfig', words = { 'lspconfig.settings' } },
+        { path = 'snacks.nvim', words = { 'Snacks' } },
       },
     },
   },
