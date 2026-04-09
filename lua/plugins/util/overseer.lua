7e72eccca07ac8840e0556affc0d08694c966bf9
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-04-09 21:22:21 +0530

    fix(overseer): Adjust the config after breaking changes in v2

--- lua/plugins/util/overseer.lua
+++ lua/plugins/util/overseer.lua
@@ -1,54 +1,68 @@
 return {
-  'stevearc/overseer.nvim',
-  cmd = {
-    'OverseerOpen',
-    'OverseerClose',
-    'OverseerToggle',
-    'OverseerSaveBundle',
-    'OverseerLoadBundle',
-    'OverseerDeleteBundle',
-    'OverseerRunCmd',
-    'OverseerRun',
-    'OverseerInfo',
-    'OverseerBuild',
-    'OverseerQuickAction',
-    'OverseerTaskAction',
-    'OverseerClearCache',
-  },
-  opts = {
-    dap = false,
-    task_list = {
-      bindings = {
-        ['<C-h>'] = false,
-        ['<C-j>'] = false,
-        ['<C-k>'] = false,
-        ['<C-l>'] = false,
+  {
+    'stevearc/overseer.nvim',
+    cmd = {
+      'OverseerOpen',
+      'OverseerClose',
+      'OverseerToggle',
+      'OverseerRun',
+      'OverseerTaskAction',
+    },
+    opts = {
+      dap = false,
+      task_list = {
+        keymaps = {
+          ['<C-j>'] = false,
+          ['<C-k>'] = false,
+        },
+      },
+      form = {
+        win_opts = {
+          winblend = 0,
+        },
+      },
+      confirm = {
+        win_opts = {
+          winblend = 0,
+        },
+      },
+      task_win = {
+        win_opts = {
+          winblend = 0,
+        },
       },
     },
-    form = {
-      win_opts = {
-        winblend = 0,
-      },
-    },
-    confirm = {
-      win_opts = {
-        winblend = 0,
-      },
-    },
-    task_win = {
-      win_opts = {
-        winblend = 0,
-      },
-    },
-  },
     -- stylua: ignore
     keys = {
-      { "<leader>ow", "<cmd>OverseerToggle<cr>",      desc = "Task list" },
-      { "<leader>oo", "<cmd>OverseerRun<cr>",         desc = "Run task" },
-      { "<leader>oq", "<cmd>OverseerQuickAction<cr>", desc = "Action recent task" },
-      { "<leader>oi", "<cmd>OverseerInfo<cr>",        desc = "Overseer Info" },
-      { "<leader>ob", "<cmd>OverseerBuild<cr>",       desc = "Task builder" },
-      { "<leader>ot", "<cmd>OverseerTaskAction<cr>",  desc = "Task action" },
-      { "<leader>oc", "<cmd>OverseerClearCache<cr>",  desc = "Clear cache" },
+      { "<leader>ow", "<cmd>OverseerToggle!<cr>",    desc = "Task list" },
+      { "<leader>oo", "<cmd>OverseerRun<cr>",        desc = "Run task" },
+      { "<leader>ot", "<cmd>OverseerTaskAction<cr>", desc = "Task action" },
     },
+  },
+  {
+    'folke/edgy.nvim',
+    optional = true,
+    opts = function(_, opts)
+      opts.right = opts.right or {}
+      table.insert(opts.right, {
+        title = 'Overseer',
+        ft = 'OverseerList',
+        open = function() require('overseer').open() end,
+      })
+    end,
+  },
+  {
+    'nvim-neotest/neotest',
+    optional = true,
+    opts = function(_, opts)
+      opts = opts or {}
+      opts.consumers = opts.consumers or {}
+      opts.consumers.overseer = require 'neotest.consumers.overseer'
+    end,
+  },
+  {
+    'mfussenegger/nvim-dap',
+    optional = true,
+    opts = function() require('overseer').enable_dap() end,
+  },
 }
