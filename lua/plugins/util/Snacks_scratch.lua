4ed4c7b9218ef5d44a1954fce47e0759a7e37d27
VigneshR387 <187846000+VigneshR387@users.noreply.github.com>
2026-04-10 11:50:07 +0530

    feat(snacks): setup scratch buffer to todo

--- lua/plugins/util/Snacks_scratch.lua
+++ lua/plugins/util/Snacks_scratch.lua
@@ -4,5 +4,22 @@ return {
     { '<leader>.', function() Snacks.scratch() end, desc = 'Toggle Scratch Buffer' },
     { '<leader>S', function() Snacks.scratch.select() end, desc = 'Select Scratch Buffer' },
     { '<leader>dps', function() Snacks.profiler.scratch() end, desc = 'Profiler Scratch Buffer' },
+    {
+      '<leader>T.',
+      function()
+        -- Can implement your own logic for saving files by cwd, project, git branch, etc.
+        local data = vim.fn.stdpath 'data'
+        local root = Util.root.get() .. '/todo'
+        vim.fn.mkdir(root, 'p')
+        local file = root .. '/todo.md' -- IMPORTANT: must match checkmate `files` pattern
+
+        ---@diagnostic disable-next-line: missing-fields
+        Snacks.scratch.open {
+          ft = 'markdown',
+          file = file,
+        }
+      end,
+      desc = 'Toggle Scratch Todo',
+    },
   },
 }
